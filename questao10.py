616c419b33df6ec7f9f748fa99d8c5df662a26e3
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-08 11:21:54 -0300

    Mudança na fórmula da porcentagem

--- questao10.py
+++ questao10.py
@@ -8,5 +8,7 @@ votos_brancos = int(input("Qual foi o número de votos brancos?: "))
 votos_nulos = int(input("Qual foi o número de votos nulos?: "))
 votos_validos = int(input("Qual foi o número de votos validos?: "))
 
+print(f"\nVotos totais: {numero}\nVotos brancos: {(100 * votos_brancos)/numero:.2f}%\nVotos nulos: {(100 * votos_nulos)/numero:.2f}%\nVotos validos: {(100 * votos_validos)/numero:.2f}%")
 
-print(f"\nVotos totais: {numero}\nVotos brancos: {(votos_brancos/numero) * 100:.2f}%\nVotos nulos: {(votos_nulos/numero) * 100:.2f}%\nVotos validos: {(votos_validos/numero) * 100:.2f}%")
+
+
