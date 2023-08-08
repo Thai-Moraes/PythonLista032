082f41ff084d9dd12bee609b9981ff573779ad58
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-08 11:11:50 -0300

    Atividades que faltavam fazer estão feitas

--- questao09.py
+++ questao09.py
@@ -3,6 +3,8 @@ Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias
 em dias. Obs: Considere os anos com 365 dias e os meses com 30 dias
 """
 
-anos = int(input("Sua idade em anos: "))
-meses = int(input("Sua idade em meses: "))
-dias = int(input("Sua idade em dias: "))
+anos = int(input("Quantos anos você tem?: "))
+meses = int(input("Quantos meses se passaram desde o seu último aniversário?: "))
+dias = int(input("Quantos dias se passaram desde o último mês?: "))
+
+print(f"Sua idade em dias é {(anos * 365) + (meses * 30) + dias}")
--- questao10.py
+++ questao10.py
@@ -2,3 +2,11 @@
  Escreva um algoritmo pergunte o número total de eleitores de um município, o número de votos brancos, nulos
 e válidos e apresente como resposta o percentual que cada um representa em relação ao total de eleitores
 """
+
+numero = int(input("Qual o número total de eleitores?: "))
+votos_brancos = int(input("Qual foi o número de votos brancos?: "))
+votos_nulos = int(input("Qual foi o número de votos nulos?: "))
+votos_validos = int(input("Qual foi o número de votos validos?: "))
+
+
+print(f"\nVotos totais: {numero}\nVotos brancos: {(votos_brancos/numero) * 100}%\nVotos nulos: {(votos_nulos/numero) * 100}%\nVotos validos: {(votos_validos/numero) * 100}%")
