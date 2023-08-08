f3ef78fc0ad7773202c97af4f46c4e122f22507f
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-08 11:26:34 -0300

    Arrumar algumas coisas

--- questao10.py
+++ questao10.py
@@ -8,7 +8,7 @@ votos_brancos = int(input("Qual foi o número de votos brancos?: "))
 votos_nulos = int(input("Qual foi o número de votos nulos?: "))
 votos_validos = int(input("Qual foi o número de votos validos?: "))
 
-print(f"\nVotos totais: {numero}\nVotos brancos: {(100 * votos_brancos)/numero:.2f}%\nVotos nulos: {(100 * votos_nulos)/numero:.2f}%\nVotos validos: {(100 * votos_validos)/numero:.2f}%")
+print(f"\nVotos totais: {numero}\nVotos brancos em relação ao total: {(100 * votos_brancos)/numero:.2f}%\nVotos nulos em relação ao total: {(100 * votos_nulos)/numero:.2f}%\nVotos validos em relação ao total: {(100 * votos_validos)/numero:.2f}%")
 
 
 
